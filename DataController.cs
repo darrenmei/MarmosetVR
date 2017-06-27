b19f6fea864480180c2d9946696ef2ebace84c85
hleou <hleou@stanford.edu>
2017-06-27 16:27:23 -0700

    Add files via upload

---
+++ DataController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.IO;                                                        // The System.IO namespace contains functions related to loading and saving files
+
+public class DataController : MonoBehaviour {
+	private LocationData[] allLocations;
+	private string locationDataFileName = "data.txt";
+	private int num_locations = 0;
+
+	void Start() {
+		LoadLocation ();
+	}
+
+	private void LoadLocation()
+	{
+		// Path.Combine combines strings into a file path
+		// Application.StreamingAssets points to Assets/StreamingAssets in the Editor, and the StreamingAssets folder in a build
+		string filePath = Path.Combine(Application.streamingAssetsPath, locationDataFileName);
+
+		if(File.Exists(filePath))
+		{
+			StreamReader reader = File.OpenText (locationDataFileName);
+			string line;
+			while ((line = reader.ReadLine ()) != null) 
+			{
+				allLocations [num_locations] = readLocation(line);
+				num_locations++;
+			}
+		}
+		else
+		{
+			Debug.LogError("Cannot load data!");
+		}
+	}
+
+	private LocationData readLocation(string line) {
+		return new LocationData (line.Split (','));
+	}
+		
+}
+	
---
+++ LocationData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public struct LocationData
+{
+	public int x, y, z;
+	public LocationData(string[] locations) {
+		x = int.Parse(locations[0]);
+		y = int.Parse(locations[1]);
+		z = int.Parse(locations[2]);
+	}
+}
+
